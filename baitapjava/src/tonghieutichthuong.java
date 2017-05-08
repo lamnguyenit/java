2baf8c083e2537dc6d004c83e2cf30bd5c4fded0
Lam nguyen IT <lamnguyen260895@gmail.com>
2017-05-08 14:29:14 +0700

    bài 2: tong-hieu-tich-thuong

---
+++ baitapjava/src/tonghieutichthuong.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LamIT
+ */
+import java.awt.*;
+import java.awt.event.*;
+public class tonghieutichthuong extends Frame implements ActionListener, WindowListener{
+    Label lblA=new Label("A=");
+    Label lblB=new Label("b=");
+    Label lblKQ=new Label("KQ");
+    TextField txtA=new TextField(10);
+    TextField txtB=new TextField(10);
+    TextField txtKQ=new TextField(20);
+    Button btnTong=new Button("Tong");
+    Button btnHieu=new Button("Hieu");
+    Button btnTich=new Button("Tich");
+    Button btnThuong=new Button("Thuong");
+    public tonghieutichthuong()
+    {
+        this.add(lblA);
+        this.add(txtA);
+        this.add(lblB);
+        this.add(txtB);
+        this.add(btnTong);
+        btnTong.addActionListener(this);
+        this.add(btnHieu);
+        btnHieu.addActionListener(this);
+        this.add(btnTich);
+        btnTich.addActionListener(this);
+        this.add(btnThuong);
+        btnThuong.addActionListener(this);
+        this.add(lblKQ);
+        this.add(txtKQ);
+        txtKQ.setEditable(false);
+        addWindowListener(this);
+        
+        
+        this.setLayout(new GridLayout(5,2,0,10));
+        this.setBackground(Color.cyan);
+        
+    }
+    public void actionPerformed(ActionEvent e)
+    {
+        if(e.getSource()==btnTong)
+        {
+            long a=Integer.parseInt(txtA.getText());
+            long b=Integer.parseInt(txtB.getText());
+            long c=a+b;
+            txtKQ.setText(c + ""); 
+        }
+         if(e.getSource()==btnHieu)
+        {
+            long a=Integer.parseInt(txtA.getText());
+            long b=Integer.parseInt(txtB.getText());
+            long c=a-b;
+            txtKQ.setText(c + ""); 
+            
+
+        }
+          if(e.getSource()==btnTich)
+        {
+            long a=Integer.parseInt(txtA.getText());
+            long b=Integer.parseInt(txtB.getText());
+            long c=a*b;
+            txtKQ.setText(c + ""); 
+            
+
+        }
+           if(e.getSource()==btnThuong)
+        {
+            long a=Integer.parseInt(txtA.getText());
+            long b=Integer.parseInt(txtB.getText());S
+            double c=(float)a/b;
+            txtKQ.setText(c + ""); 
+            
+
+        }
+    }
+    public static void main(String[] args) {
+        tonghieutichthuong thtt=new tonghieutichthuong();
+        thtt.setTitle("tonghieutichthuong");
+        thtt.setSize(300,200);
+        
+        thtt.setVisible(true);
+    }
+
+    @Override
+   public void windowClosing(WindowEvent evt) {
+      System.exit(0);  // Terminate the program
+   }
+ 
+   // Not Used, but need to provide an empty body to compile.
+   @Override public void windowOpened(WindowEvent evt) { }
+   @Override public void windowClosed(WindowEvent evt) { }
+   @Override public void windowIconified(WindowEvent evt) { }
+   @Override public void windowDeiconified(WindowEvent evt) { }
+   @Override public void windowActivated(WindowEvent evt) { }
+   @Override public void windowDeactivated(WindowEvent evt) { }
+}
